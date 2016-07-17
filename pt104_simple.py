6779769fe504b28e3254a939702f64afaf8945eb
kenike88 <dennisong.wh@gmail.com>
2016-07-17 18:11:14 +0800

    Add files via upload

---
+++ pt104_simple.py
@@ -0,0 +1,32 @@
+import ctypes
+import numpy as np
+import time
+
+temp1 = np.zeros((1,), dtype=np.int32)
+temp2 = np.zeros((1,), dtype=np.int32)
+temp3 = np.zeros((1,), dtype=np.int32)
+temp0 = np.int32
+mydll = ctypes.windll.LoadLibrary('UsbPt104.dll')
+handlePointer = ctypes.c_short()
+# temp=np.zeros((9,),dtype=np.int64)
+status_unit = mydll.UsbPt104OpenUnit(ctypes.byref(handlePointer), 'CR944/112')
+print 'open unit status:', status_unit
+for j in range(1, 4):
+    status_channel = mydll.UsbPt104SetChannel(handlePointer, j, 1, 2)
+    print 'open channel status', status_channel
+
+def pid(temp):
+    p = 5
+    sp = 20
+    duty_cycle = p*(temp - sp)
+    return duty_cycle
+
+for i in range(0, 500):
+    mydll.UsbPt104GetValue(handlePointer, 1, temp1.ctypes.data, False)
+    mydll.UsbPt104GetValue(handlePointer, 2, temp2.ctypes.data, False)
+    mydll.UsbPt104GetValue(handlePointer, 3, temp3.ctypes.data, False)
+    dc = pid(temp2[0] / 1000.0)
+    print temp1 / 1000.0, temp2 / 1000.0, temp3 / 1000.0, dc,'%'
+
+    time.sleep(0.1)
+
